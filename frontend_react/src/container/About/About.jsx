c4690cfb37ef2f5e310a803f62065a3866b8b8f8
yuvraj <yuvi.unique@gmail.com>
2023-03-08 09:03:35 +0530

    Added about page design layout

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -4,7 +4,7 @@ import { images } from '../../constants'
 import './About.scss'
 
 const abouts = [
-  { title: 'Web Developer', description: 'I am a web developer with a passion for building beautiful and functional web applications and also maintain websites and web applications.', imgUrl: images.about01 },
+  { title: 'API Developer', description: 'I am a API (application programming interface) developer with a passion for developing APIs that are essential in app development across multiple devices, databases and servers.', imgUrl: images.about01 },
   { title: 'Frontend Developer', description: 'I am a frontend developer with a passion for creating websites and applications using Reactjs and Nextjs that allow users to access and interact with the site or app.', imgUrl: images.about02 },
   { title: 'Backend Developer', description: 'I am a backend developer with a passion for building code that allows a database and an application to communicate with one another.', imgUrl: images.about03 },
   { title: 'Full Stack Developer', description: 'I am a full stack developer with a passion for building both the front end and the back end of a website and also manages both front and back end systems.', imgUrl: images.about04 },
@@ -14,7 +14,7 @@ const About = () => {
     <>
       <h2 className='head-text'>
         I know that
-        <span> Good Development</span>
+        <span> Good Website</span>
         <br />
         means
         <span> Good Business</span>
